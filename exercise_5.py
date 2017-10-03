78a24fdf73c53c6ee52d14350a85b7cb12064188
Ollie Beakbane <obeakbane@ollies-mbp.home>
2017-10-03 20:45:27 +0100

    First commit

---
+++ exercise_5.py
@@ -0,0 +1,13 @@
+#Create a class
+
+class InpOutString(obj):
+    def __init__(self):
+        self.s = ""
+
+    def getString(self):
+        self.s = input('> ')
+
+    def printString(self):
+        print(self.s.upper())
+
+strObj =
